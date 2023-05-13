6a5ef5ebfa8fa68d69b1f47dab27025530f3ea18
AmitBerko <amit.berko10@gmail.com>
2023-05-13 21:40:11 +0300

    Updated formatting

--- src/CalculatorContext.js
+++ src/CalculatorContext.js
@@ -1,45 +1,51 @@
-import React, { useState } from 'react';
+import React, { useState } from 'react'
 
-const CalculatorContext = React.createContext();
+// Create a new context for the calculator
+const CalculatorContext = React.createContext()
 
 const CalculatorProvider = ({ children }) => {
-    const [moves, setMoves] = useState(0);
-    const [goal, setGoal] = useState(0);
-    const [result, setResult] = useState(0);
-    const [initialMoves, setInitialMoves] = useState(0);
-    const [initialResult, setInitialResult] = useState(0);
-    const [initialGoal, setInitialGoal] = useState(0);
+    // Define the states
+    const [moves, setMoves] = useState(0)
+    const [goal, setGoal] = useState(0)
+    const [result, setResult] = useState(0)
+    const [initialMoves, setInitialMoves] = useState(0)
+    const [initialResult, setInitialResult] = useState(0)
+    const [initialGoal, setInitialGoal] = useState(0)
     const [buttons, setButtons] = useState([
         { id: 0, type: 'emptyButton' },
         { id: 1, type: 'emptyButton' },
         { id: 2, type: 'clrButton' },
         { id: 3, type: 'emptyButton' },
         { id: 4, type: 'emptyButton' },
-        { id: 5, type: 'operatorButton', operator: '+', value: 50 },
+        { id: 5, type: 'emptyButton' },
         { id: 6, type: 'emptyButton' },
-        { id: 7, type: 'addDigitButton', value: 5 },
-        { id: 8, type: 'specialButton', specialType: 'mirror' },
+        { id: 7, type: 'emptyButton' },
+        { id: 8, type: 'emptyButton' },
     ])
 
+    // Function to update the initial values based on user input
     const updateInitialValues = () => {
-        let moves = parseInt(document.getElementById('initial-moves').value)
-        let result = parseInt(document.getElementById('initial-result').value)
-        let goal = parseInt(document.getElementById('goal-input').value)
-        if (goal == result || moves == 0 || isNaN(moves) || isNaN(result) || isNaN(goal)) {
+        const moves = parseInt(document.getElementById('initial-moves').value)
+        const result = parseInt(document.getElementById('initial-result').value)
+        const goal = parseInt(document.getElementById('goal-input').value)
+
+        // Handle invalid input cases
+        if (goal === result || moves === 0 || isNaN(moves) || isNaN(result) || isNaN(goal)) {
             setResult('Bad Input')
             return
         }
 
-        setMoves(moves);
-        setResult(result);
-        setGoal(goal);
-        setInitialMoves(moves);
-        setInitialResult(result);
-        setInitialGoal(goal);
-
-    };
+        // Update the state variables with the new values
+        setMoves(moves)
+        setResult(result)
+        setGoal(goal)
+        setInitialMoves(moves)
+        setInitialResult(result)
+        setInitialGoal(goal)
+    }
 
     return (
+        // Provide the state variables and setters through the context
         <CalculatorContext.Provider
             value={{
                 moves,
@@ -62,31 +68,23 @@ const CalculatorProvider = ({ children }) => {
                 <h1>Level Settings</h1>
                 <div className="input-group">
                     <label htmlFor="initial-moves">Initial Moves:</label>
-                    <input
-                        type="number"
-                        id="initial-moves"
-                    />
+                    <input type="number" id="initial-moves" />
                 </div>
                 <div className="input-group">
                     <label htmlFor="initial-result">Initial Result:</label>
-                    <input
-                        type="number"
-                        id="initial-result"
-                    />
+                    <input type="number" id="initial-result" />
                 </div>
                 <div className="input-group">
                     <label htmlFor="goal-input">Goal:</label>
-                    <input
-                        type="number"
-                        id="goal-input"
-                    />
+                    <input type="number" id="goal-input" />
                 </div>
-                <button className="update" onClick={updateInitialValues}>Update</button>
+                <button className="update" onClick={updateInitialValues}>
+                    Update
+                </button>
             </div>
             {children}
         </CalculatorContext.Provider>
-    );
-};
+    )
+}
 
-
-export { CalculatorContext, CalculatorProvider };
+export { CalculatorContext, CalculatorProvider }
